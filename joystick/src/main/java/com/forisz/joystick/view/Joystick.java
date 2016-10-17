9e0d1300de2908a7064cbee96faf4578302c1515
Forisz <forisz.zisis@remek.com>
2016-10-17 10:35:32 +0200

    Jojstick View update Added public and private constants; New method that helps drawing the joystick properly. New interface for handling events;

--- joystick/src/main/java/com/forisz/joystick/view/Joystick.java
+++ joystick/src/main/java/com/forisz/joystick/view/Joystick.java
@@ -2,19 +2,91 @@ package com.forisz.joystick.view;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PointF;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 
 public class Joystick extends View {
+    private static final String TAG = Joystick.class.getSimpleName();
 
-    public static final int JOYSTICK_MODE_NORMAL = 1;
-    public static final int JOYSTICK_MODE_STATIC = 2;
+    ///////////////
+    // BEHAVIORS //
+    ///////////////
+    /**
+     * Normal joystick behavior. The inner circle returns to the center when the user releases the view
+     */
+    public static final int BEHAVIOR_NORMAL = 1;
 
-    private static final int DEFAULT_JOYSTICK_MODE = JOYSTICK_MODE_NORMAL;
-    private static final int DEFAULT_JOYSTICK_BACKGROUND_COLOR = Color.RED;
+    /**
+     * Static joystick behavior. The inner circle stays in position when the user releases the view
+     */
+    public static final int BEHAVIOR_STATIC = 2;
+
+    /////////////////
+    // APPEARANCES //
+    /////////////////
+    /**
+     * Draw a regular joystick with outer and inner circles and a line connecting them
+     */
+    public static final int APPEARANCE_NORMAL = 1;
+
+    /**
+     * Draw a joystic without the outer circle
+     */
+    public static final int APPEARANCE_NO_BASE = 2;
+
+    /**
+     * Draw a joystick with only the inner circle
+     */
+    public static final int APPEARANCE_INNER_CIRCLE_ONLY = 3;
+
+    
+    private static final int DEFAULT_MODE = BEHAVIOR_NORMAL;
+    private static final int DEFAULT_APPEARANCE = APPEARANCE_NORMAL;
+    private static final int DEFAULT_OUTER_CIRCLE_WIDTH = 5;
+    private static final int DEFAULT_INNER_CIRCLE_RADIUS = 50;
+    private static final int DEFAULT_INNER_CIRCLE_COLOR = Color.RED;
+    private static final int DEFAULT_OUTER_CIRCLE_COLOR = Color.BLACK;
+    private static final int DEFAULT_LINE_COLOR = Color.BLUE;
+    private static final int DEFAULT_LINE_WIDTH = 2;
+    private static final Paint.Style DEFAULT_INNER_CIRCLE_STYLE = Paint.Style.FILL;
+    private static final Paint.Style DEFAULT_OUTER_CIRCLE_STYLE = Paint.Style.FILL;
+
+    /**
+     * Listener for handling joystick events
+     */
+    private OnJoysticEventListener listener;
+
+    /**
+     * Paint instance for drawing the inner circle of the joystick
+     */
+    private Paint innerCirclePaint;
+
+    /**
+     * Paint instance for drawing the outer circle of the joystick
+     */
+    private Paint outerCirclePaint;
+
+    /**
+     * Paint instance for drawing the line that connects the inner and outer circles of the joystick
+     */
+    private Paint linePaint;
+
+    private int drawMode;
+    private int behavior;
+    private int innerCircleRadius;
+    private int outerCircleRadius;
+    private int lineWidth;
+
+    private int innerCircleBorderWidth;
+    private int outerCircleBorderWidth;
+
 
 
     public Joystick(Context context) {
@@ -40,10 +112,72 @@ public class Joystick extends View {
 
     /**
      * Initialise Joystick
+     *
      * @param context
      * @param attrs
      */
     private void init(Context context, AttributeSet attrs) {
 
     }
+
+    @Override
+    public boolean isInEditMode() {
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
+    /**
+     * Calculates the intersecting points of a line and a circle.
+     * Used for preventing the outer circle from moving outside of the bounds of the outer circle
+     * @param pointA First point of the line
+     * @param pointB Second point of the line
+     * @param center The center of the circle
+     * @param radius The radius of the circle
+     * @return The intersection of the circle and the line
+     */
+    private PointF getCircleLineIntersection(PointF pointA, PointF pointB, PointF center, float radius) {
+        // Calculate pointB - pointA and center - pointA offsets on the X and Y axis
+        float baX = pointB.x - pointA.x;
+        float baY = pointB.y - pointA.y;
+        float caX = center.x - pointA.x;
+        float caY = center.y - pointA.y;
+
+        float a = (baX * baX) + (baY * baY);
+        float bBy2 = (baX * caX) + (baY * caY);
+        float c = ((caX * caX) + (caY * caY)) - (radius * radius);
+
+        float pBy2 = bBy2 / a;
+        float q = c / a;
+
+        float disc = (pBy2 * pBy2) - q;
+
+        float abScalingFactor1 = -pBy2 + (float)Math.sqrt(disc);
+
+        return new PointF(pointA.x - baX * abScalingFactor1, pointA.y - baY * abScalingFactor1);
+    }
+
+
+    public void setOnJoystickEventListener(OnJoysticEventListener listener) {
+        this.listener = listener;
+    }
+    interface OnJoysticEventListener {
+        void onMove();
+        void onRelease();
+        void onTouch();
+    }
+
 }
--- sample/src/main/java/com/forisz/android_joystick/MainActivity.java
+++ sample/src/main/java/com/forisz/android_joystick/MainActivity.java
@@ -2,12 +2,19 @@ package com.forisz.android_joystick;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.forisz.joystick.view.Joystick;
 
 public class MainActivity extends AppCompatActivity {
 
+    Joystick joy1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Log.i("asd", Integer.toString(Joystick.DRAW_MODE_INNER_CIRCLE_ONLY));
     }
 }
