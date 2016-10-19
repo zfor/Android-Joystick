d046b3af477cc03ae50fa00524caf185f8914f8c
Forisz <forisz.zisis@remek.com>
2016-10-20 00:16:49 +0200

    Implemented onDraw and touch event handling

--- joystick/src/main/java/com/forisz/joystick/view/Joystick.java
+++ joystick/src/main/java/com/forisz/joystick/view/Joystick.java
@@ -1,15 +1,21 @@
 package com.forisz.joystick.view;
 
+import android.animation.ValueAnimator;
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.PointF;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+
+import com.forisz.joystick.R;
 
 
 public class Joystick extends View {
@@ -45,12 +51,22 @@ public class Joystick extends View {
      * Draw a joystick with only the inner circle
      */
     public static final int APPEARANCE_INNER_CIRCLE_ONLY = 3;
-
-    
-    private static final int DEFAULT_MODE = BEHAVIOR_NORMAL;
     private static final int DEFAULT_APPEARANCE = APPEARANCE_NORMAL;
+    private static final int DEFAULT_BEHAVIOR = BEHAVIOR_NORMAL;
     private static final int DEFAULT_OUTER_CIRCLE_WIDTH = 5;
-    private static final int DEFAULT_INNER_CIRCLE_RADIUS = 50;
+
+    /**
+     * Default radius of the inner circle in display-independent pixels
+     * 0 means auto
+     */
+    private static final int DEFAULT_INNER_CIRCLE_RADIUS = 0;
+
+    /**
+     * Default radius of the outer circle in display-independent pixels
+     * 0 means auto
+     */
+    private static final int DEFAULT_OUTER_CIRCLE_RADIUS = 0;
+
     private static final int DEFAULT_INNER_CIRCLE_COLOR = Color.RED;
     private static final int DEFAULT_OUTER_CIRCLE_COLOR = Color.BLACK;
     private static final int DEFAULT_LINE_COLOR = Color.BLUE;
@@ -78,15 +94,35 @@ public class Joystick extends View {
      */
     private Paint linePaint;
 
-    private int drawMode;
+    private int appearance;
     private int behavior;
     private int innerCircleRadius;
     private int outerCircleRadius;
     private int lineWidth;
-
     private int innerCircleBorderWidth;
     private int outerCircleBorderWidth;
 
+    /**
+     * The center of the view on the X axis (also the center of the outer circle)
+     */
+    private int centerX;
+
+    /**
+     * The center of the view on the Y axis (also the center of the outer circle)
+     */
+    private int centerY;
+
+    /**
+     * The center of the inner circle on the X axis
+     */
+    private int joyX;
+
+    /**
+     * The center of the inner circle on the Y axis
+     */
+    private int joyY;
+
+    private ValueAnimator animation;
 
 
     public Joystick(Context context) {
@@ -117,6 +153,43 @@ public class Joystick extends View {
      * @param attrs
      */
     private void init(Context context, AttributeSet attrs) {
+        TypedArray attributes = null;
+        int innerCircleColor;
+        int outerCircleColor;
+        int lineColor;
+        try {
+            attributes = context.getTheme().obtainStyledAttributes(attrs, R.styleable.Joystick, 0, 0);
+
+            appearance = attributes.getInt(R.styleable.Joystick_appearance, DEFAULT_APPEARANCE);
+            behavior = attributes.getInt(R.styleable.Joystick_behavior, DEFAULT_BEHAVIOR);
+            innerCircleRadius = attributes.getDimensionPixelSize(R.styleable.Joystick_inner_circle_radius, DEFAULT_INNER_CIRCLE_RADIUS);
+            outerCircleRadius = attributes.getDimensionPixelSize(R.styleable.Joystick_outer_circle_radius, DEFAULT_OUTER_CIRCLE_RADIUS);
+            innerCircleColor = attributes.getColor(R.styleable.Joystick_inner_circle_color, DEFAULT_INNER_CIRCLE_COLOR);
+            outerCircleColor = attributes.getColor(R.styleable.Joystick_outer_circle_color, DEFAULT_OUTER_CIRCLE_COLOR);
+
+        } finally {
+            if (attributes != null) {
+                attributes.recycle();
+            }
+        }
+
+        // Set up Paint instances for onDraw
+
+        outerCirclePaint = new Paint();
+        outerCirclePaint.setStyle(Paint.Style.FILL);
+        outerCirclePaint.setColor(outerCircleColor);
+        outerCirclePaint.setAntiAlias(true);
+
+        innerCirclePaint = new Paint();
+        innerCirclePaint.setStyle(Paint.Style.FILL);
+        innerCirclePaint.setColor(innerCircleColor);
+        innerCirclePaint.setAntiAlias(true);
+
+        linePaint = new Paint();
+        linePaint.setStyle(Paint.Style.STROKE);
+        linePaint.setColor(Color.BLACK);
+        linePaint.setAntiAlias(true);
+        linePaint.setStrokeWidth(5f);
 
     }
 
@@ -128,21 +201,104 @@ public class Joystick extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        // Draw the outer circle
+        canvas.drawCircle(centerX, centerY, outerCircleRadius, outerCirclePaint);
+        // Draw the line connecting the inner and outer circles
+     //   canvas.drawLine(joyX, joyY, centerX, centerY, linePaint);
+        // Draw the inner circle
+        canvas.drawCircle(joyX, joyY, innerCircleRadius, innerCirclePaint);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+
+        centerX = w / 2;
+        centerY = h / 2;
+        joyX = centerX;
+        joyY = centerY;
+
+        outerCircleRadius = (w < h ? w : h) / 2;
+        innerCircleRadius = (int) (0.4 * outerCircleRadius);
+
+        outerCircleRadius -= innerCircleRadius;
+
+
+        this.invalidate();
+
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
+        switch (event.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                handleActionDown(event);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                handleActionMove(event);
+                break;
+            case MotionEvent.ACTION_UP:
+                handleActionUp(event);
+                break;
+        }
+        return true;
+    }
+
+    private void handleActionDown(MotionEvent event) {
+        if (listener != null) {
+            listener.onTouch();
+        }
+        handleActionMove(event);
+    }
+
+    private void handleActionMove(MotionEvent event) {
+        final PointF pointer = new PointF(event.getX(0), event.getY(0));
+        final PointF center = new PointF(centerX, centerY);
+
+        // Prevent the inner circle from leaving the outer circle's boundaries.
+        PointF joy = null;
+        if (getDistance(pointer, center) > outerCircleRadius) {
+            joy = getCircleLineIntersection(pointer, center, center, outerCircleRadius);
+        } else {
+            joy = getCircleLineIntersection(pointer, center, center, getDistance(pointer, center));
+        }
+        joyX = (int) joy.x;
+        joyY = (int) joy.y;
+
+        // Let's translate the x and y position into the [-1, 1] range and call the callback with it;
+        if (listener != null) {
+            float x = joy.x / outerCircleRadius - 1;
+            float y = -(joy.y / outerCircleRadius - 1);
+            double deg = Math.toDegrees(Math.atan2(y, x));
+
+            if (deg < 0) deg = Math.abs(deg) + 180;
+
+            //listener.onMove(x, y, (float) deg);
+        }
+        Log.d(TAG, String.format("X: %d; Y: %d;", joyX, joyY));
+        this.invalidate();
+    }
+
+    private float getDistance(PointF a, PointF b) {
+        return (float)Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y , 2));
+    }
+
+    private void handleActionUp(MotionEvent event) {
+        if (behavior == BEHAVIOR_NORMAL) {
+            joyX = centerX;
+            joyY = centerY;
+            invalidate();
+        } else {
+            // STATIC
+
+        }
     }
 
     /**
      * Calculates the intersecting points of a line and a circle.
      * Used for preventing the outer circle from moving outside of the bounds of the outer circle
+     *
      * @param pointA First point of the line
      * @param pointB Second point of the line
      * @param center The center of the circle
@@ -165,7 +321,7 @@ public class Joystick extends View {
 
         float disc = (pBy2 * pBy2) - q;
 
-        float abScalingFactor1 = -pBy2 + (float)Math.sqrt(disc);
+        float abScalingFactor1 = -pBy2 + (float) Math.sqrt(disc);
 
         return new PointF(pointA.x - baX * abScalingFactor1, pointA.y - baY * abScalingFactor1);
     }
@@ -174,9 +330,22 @@ public class Joystick extends View {
     public void setOnJoystickEventListener(OnJoysticEventListener listener) {
         this.listener = listener;
     }
-    interface OnJoysticEventListener {
-        void onMove();
+
+    public interface OnJoysticEventListener {
+        /**
+         * Called when the joystick button is moved by the user
+         */
+        void onMove(int posX, int posY, int deg);
+
+        /**
+         * Called when the user releases the joystick
+         */
         void onRelease();
+
+        /**
+         * Called when the user touched the joystick.
+         * Immediately followed by at least one onMove call!
+         */
         void onTouch();
     }
 
--- sample/src/main/java/com/forisz/android_joystick/MainActivity.java
+++ sample/src/main/java/com/forisz/android_joystick/MainActivity.java
@@ -14,7 +14,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        joy1 = (Joystick) findViewById(R.id.joystick);
 
-        Log.i("asd", Integer.toString(Joystick.DRAW_MODE_INNER_CIRCLE_ONLY));
+        joy1.setOnJoystickEventListener(new Joystick.OnJoysticEventListener() {
+
+            @Override
+            public void onMove(int posX, int posY, int deg) {
+                Log.d("MainActivity", "onMove");
+            }
+
+            @Override
+            public void onRelease() {
+                Log.d("MainActivity", "onRelease");
+            }
+
+            @Override
+            public void onTouch() {
+                Log.d("MainActivity", "onTouch");
+            }
+        });
+
+
+        //Log.i("asd", Integer.toString(Joystick.DRAW_MODE_INNER_CIRCLE_ONLY));
     }
 }
